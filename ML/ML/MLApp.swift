a9cf06244833d23e2ee78448b2b47f9e3cc3ff77
Tomoka Kurihara <TomokaKurihara@kuriharayuutonoMacBook.local>
2024-10-26 10:56:47 +0900

    ML_app

---
+++ ML/ML/MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLApp.swift
+//  ML
+//
+//  Created by Tomoka Kurihara on R 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
