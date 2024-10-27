d87e2cd65c98f6957efee5783adcd4333e0ffd4a
“shin5trp” <shin5trp@gmail.com>
2024-10-27 12:10:19 +0900

    read

---
+++ ml_yolo/BreakfastFinder/ClassSelectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ClassSelectionViewController.swift
+//  BreakfastFinder
+//
+//  Created by Shingo Okayama on 2024/10/27.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
